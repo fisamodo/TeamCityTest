5002685f10a0df350bbcfd8b14dd301ee76d942b
Filip Velebit Samodol <fisamodo@vsite.hr>
2020-11-02 18:35:48 +0100

    Add project files.

---
+++ UnitTestProject/TestSquared.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.Pood.TeamCityTest;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class TestSquared
+    {
+        [TestMethod]
+        public void SquaredReturns4ForArgument2()
+        {
+            Assert.AreEqual(4, MyMath.Squared(2));
+        }
+        [TestMethod]
+        public void SquaredReturns0ForArgument0()
+        {
+            Assert.AreEqual(0, MyMath.Squared(0));
+        }
+        [TestMethod]
+        public void SquaredReturns4ForArgumentMinus2()
+        {
+            Assert.AreEqual(4, MyMath.Squared(-2));
+        }
+    }
+}
